9a4c48897a8c822d4f3491f5c877f91bb00eb4f8
D G Shivu <dgshivu3232@gmail.com>
2022-05-05 23:53:11 +0530

    feat: utility to query hasura

---
+++ src/utils/hasura.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const config = require('../config/config');
+
+/**
+ * This is a utility function for querying the postgresql database
+ * @param {string} queryString
+ * @param {object} variables
+ * @returns {Object}
+ */
+async function query(queryString, variables = {}) {
+  const result = await axios
+    .post(
+      config.hasuraApi,
+      { queryString, variables },
+      {
+        headers: {
+          'content-type': 'application/json',
+          'x-hasura-admin-secret': config.hasuraAdminSecret,
+        },
+      }
+    )
+    .then((response) => {
+      const responseData = response.data;
+
+      if (responseData.data) {
+        return {
+          success: true,
+          result: responseData,
+        };
+      }
+      return {
+        success: false,
+        errors: responseData.errors,
+      };
+    })
+    .catch((err) => {
+      return {
+        success: false,
+        errors: err,
+      };
+    });
+
+  return result;
+}
+
+module.exports = { query };
