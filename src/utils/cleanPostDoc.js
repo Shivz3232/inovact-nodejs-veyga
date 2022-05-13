93a5fef6c23eb168c89808af8862eb430c915a54
D G Shivu <dgshivu3232@gmail.com>
2022-05-13 22:32:00 +0530

    chore: fixed formatting

--- src/utils/cleanPostDoc.js
+++ src/utils/cleanPostDoc.js
@@ -1,15 +1,15 @@
 function cleanPostDoc(postDoc) {
-    postDoc.project_likes = postDoc.project_likes.result.count;
-    postDoc.has_liked = postDoc.has_liked.result.count == 1 ? true : false;
+  postDoc.project_likes = postDoc.project_likes.result.count;
+  postDoc.has_liked = postDoc.has_liked.result.count === 1;
 
-    return {
-        ...postDoc,
-        project_tags: postDoc.project_tags.map(post_tag => {
-            return {
-                name: post_tag.hashtag.name,
-            };
-        }),
-    };
+  return {
+    ...postDoc,
+    project_tags: postDoc.project_tags.map((post_tag) => {
+      return {
+        name: post_tag.hashtag.name,
+      };
+    }),
+  };
 }
 
 module.exports = cleanPostDoc;
--- src/utils/cleanThoughtDoc.js
+++ src/utils/cleanThoughtDoc.js
@@ -1,8 +1,8 @@
 function cleanThoughtDoc(thought) {
-    thought.thought_likes = thought.thought_likes.result.count;
-    thought.has_liked = thought.has_liked.result.count == 1 ? true : false;
+  thought.thought_likes = thought.thought_likes.result.count;
+  thought.has_liked = thought.has_liked.result.count === 1;
 
-    return thought;
+  return thought;
 }
 
 module.exports = cleanThoughtDoc;
