1fb3329ae28ded67089fae9454e067aeb5f46663
D G Shivu <dgshivu3232@gmail.com>
2022-05-13 22:14:19 +0530

    chore: fixed api of fetching posts

--- src/routes/v1/post.route.js
+++ src/routes/v1/post.route.js
@@ -2,14 +2,14 @@ const express = require('express');
 // const auth = require('../../middlewares/auth');
 // const validate = require('../../middlewares/validate');
 // const userValidation = require('../../validations/user.validation');
-const postController = require('../../controllers/posts.controller');
+const postController = require('../../controllers/post.controller');
 
 const router = express.Router();
 
 router
-    .route('/')
-    // .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
-    .get(postController.getPosts);
+  .route('/')
+  // .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
+  .post(postController.getPosts);
 
 // router
 //   .route('/:userId')
