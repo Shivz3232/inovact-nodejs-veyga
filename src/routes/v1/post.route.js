e6e5db3acdaaa307df15808a1910a4eb50603b94
D G Shivu <dgshivu3232@gmail.com>
2022-05-16 17:43:38 +0530

    chore: corrected module name during import

--- src/routes/v1/post.route.js
+++ src/routes/v1/post.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const auth = require('../../middlewares/auth');
 // const validate = require('../../middlewares/validate');
 // const userValidation = require('../../validations/user.validation');
-const postController = require('../../controllers/posts.controller');
+const postController = require('../../controllers/post.controller');
 
 const router = express.Router();
 
