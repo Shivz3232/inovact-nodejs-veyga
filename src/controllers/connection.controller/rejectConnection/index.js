8ed324aaf70c1764d82f7ab3f2dff20433f02ae0
D G Shivu <dgshivu3232@gmail.com>
2022-05-16 19:00:37 +0530

    bugfix: added missing returns

--- src/controllers/connection.controller/rejectConnection/index.js
+++ src/controllers/connection.controller/rejectConnection/index.js
@@ -3,7 +3,7 @@ const { query: Hasura } = require('../../../utils/hasura');
 const { getUserId, getPendingConnection, deleteConnection } = require('./queries/queries');
 
 const rejectConnection = catchAsync(async (req, res) => {
-  const { user_id, cognito_sub } = req.query;
+  const { user_id, cognito_sub } = req.body;
 
   // Find user id
   const response1 = await Hasura(getUserId, {
@@ -27,7 +27,7 @@ const rejectConnection = catchAsync(async (req, res) => {
   const response2 = await Hasura(getPendingConnection, variables);
 
   if (!response2.success)
-    res.json({
+    return res.json({
       success: false,
       errorCode: 'InternalServerError',
       errorMessage: JSON.stringify(response2.errors),
@@ -37,7 +37,7 @@ const rejectConnection = catchAsync(async (req, res) => {
   const response3 = await Hasura(deleteConnection, variables);
 
   if (!response3.success)
-    res.json({
+    return res.json({
       success: false,
       errorCode: 'InternalServerError',
       errorMessage: JSON.stringify(response3.errors),
