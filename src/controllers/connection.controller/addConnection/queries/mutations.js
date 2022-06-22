bd3a3585fed67f248d7c1b853f1d18b93bb45569
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2022-06-22 20:55:00 +0530

    bugfix: added id in returned fields

--- src/controllers/connection.controller/addConnection/queries/mutations.js
+++ src/controllers/connection.controller/addConnection/queries/mutations.js
@@ -1,6 +1,7 @@
 const addConnection = `mutation addConnection($user1: Int, $user2: Int) {
     insert_connections(objects: [{user1: $user1, user2: $user2}]) {
       returning {
+        id
         status
       }
     }
@@ -8,5 +9,5 @@ const addConnection = `mutation addConnection($user1: Int, $user2: Int) {
   `;
 
 module.exports = {
-    addConnection,
+  addConnection,
 };
