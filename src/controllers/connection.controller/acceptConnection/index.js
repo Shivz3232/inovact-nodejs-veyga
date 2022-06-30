55298da87ca9a2e45c515d2d0709f2903e2f1109
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2022-06-30 21:23:13 +0530

    chore: added notification

--- src/controllers/connection.controller/acceptConnection/index.js
+++ src/controllers/connection.controller/acceptConnection/index.js
@@ -2,6 +2,8 @@ const { query: Hasura } = require('../../../utils/hasura');
 const { getUserId, getPendingConnection } = require('./queries/queries');
 const { acceptConnection: acceptConnectionQuery } = require('./queries/mutations');
 const catchAsync = require('../../../utils/catchAsync');
+const notify = require('../../../utils/notify');
+const logger = require('../../../config/logger');
 
 const acceptConnection = catchAsync(async (req, res) => {
   const { user_id, cognito_sub } = req.body;
@@ -36,6 +38,15 @@ const acceptConnection = catchAsync(async (req, res) => {
       data: null,
     });
 
+  if (response2.result.data.connections.length === 0 || response2.result.data.connections[0].status !== 'pending') {
+    return res.json({
+      success: false,
+      errorCode: 'ConnectionNotFoundError',
+      errorMessage: 'Connection not found',
+      data: null,
+    });
+  }
+
   variables.formedAt = new Date().toISOString();
 
   const response3 = await Hasura(acceptConnectionQuery, variables);
@@ -47,6 +58,11 @@ const acceptConnection = catchAsync(async (req, res) => {
       errorMessage: JSON.stringify(response3.errors),
       data: null,
     });
+
+  // Notify the user
+  await notify(17, response2.result.data.connections[0].id, response2.result.data.connections[0].user2, [user_id]).catch(
+    logger.debug
+  );
 
   res.json({
     success: true,
--- src/controllers/connection.controller/acceptConnection/queries/queries.js
+++ src/controllers/connection.controller/acceptConnection/queries/queries.js
@@ -7,6 +7,7 @@ const getUserId = `query getUser($cognito_sub: String_comparison_exp) {
 
 const getPendingConnection = `query getConnection($user1: Int, $user2: Int) {
     connections(where: { _and: [{user1: { _eq: $user1 }}, {user2: { _eq: $user2 }}], status: { _eq: "pending"}}) {
+      id
       user1
       user2
       status
