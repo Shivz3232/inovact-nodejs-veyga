da00b2b474a192ac8ca134b9b5bf9bc01095423b
Nabeelcodes110 <parvenabeel@gmail.com>
2023-05-13 20:58:09 +0530

    Added The updated Queries

--- src/controllers/idea.controller/deleteIdea/queries/queries.js
+++ src/controllers/idea.controller/deleteIdea/queries/queries.js
@@ -1,5 +1,8 @@
 const delete_idea = `
 mutation delete_idea($id: Int!) {
+  delete_team(where :{ideas : {id : {_eq :$id}}}){
+    affected_rows
+  }
   delete_idea_by_pk(id: $id) {
     id
   }
--- src/controllers/idea.controller/getIdea/queries/queries.js
+++ src/controllers/idea.controller/getIdea/queries/queries.js
@@ -1,129 +1,129 @@
 const getIdeas = `query getIdeas($cognito_sub: String) {
-    idea(order_by: {created_at:desc}) {
+  idea(order_by: {created_at:desc} ,where :{user : {status : {_neq:0}}}) {
+    id
+    title
+    description
+    user_id
+    team_id
+    status
+    link
+    idea_tags {
+      hashtag {
+        name
+      }
+    }
+    idea_likes: idea_likes_aggregate {
+      result: aggregate {
+        count
+      }
+    }
+    has_liked: idea_likes_aggregate (where: { user: { cognito_sub: {_eq: $cognito_sub}}}) {
+      result: aggregate {
+        count
+      }
+    }
+    idea_comments {
       id
-      title
-      description
-      user_id
-      team_id
-      status
-      link
-      idea_tags {
-        hashtag {
-          name
-        }
-      }
-      idea_likes: idea_likes_aggregate {
-        result: aggregate {
-          count
-        }
-      }
-      has_liked: idea_likes_aggregate (where: { user: { cognito_sub: {_eq: $cognito_sub}}}) {
-        result: aggregate {
-          count
-        }
-      }
-      idea_comments {
-        id
-        created_at
-        text
-        updated_at
-        user {
-          id
-          first_name
-          last_name
-          avatar
-        }
-      }
       created_at
+      text
       updated_at
       user {
         id
-        avatar
         first_name
         last_name
-        role
-      }
-      team {
-        id
-        looking_for_members
-        looking_for_mentors
+        avatar
       }
     }
+    created_at
+    updated_at
+    user {
+      id
+      avatar
+      first_name
+      last_name
+      role
+    }
+    team {
+      id
+      looking_for_members
+      looking_for_mentors
+    }
   }
-  `;
+}
+`;
 
 const getIdea = `query getIdea($id: Int, $cognito_sub: String) {
-    idea (where: { id: { _eq: $id }}) {
+  idea (where: {  id :{_eq: $id}  , user :{status :{_neq:0}}}) {
+    id
+    title
+    description
+    team_id
+    user_id
+    status
+    link
+    idea_tags {
+      hashtag {
+        name
+      }
+    }
+    idea_likes: idea_likes_aggregate {
+      result: aggregate {
+        count
+      }
+    }
+    has_liked: idea_likes_aggregate (where: { user: { cognito_sub: {_eq: $cognito_sub}}}) {
+      result: aggregate {
+        count
+      }
+    }
+    idea_comments {
       id
-      title
-      description
-      team_id
-      user_id
-      status
-      link
-      idea_tags {
-        hashtag {
-          name
-        }
-      }
-      idea_likes: idea_likes_aggregate {
-        result: aggregate {
-          count
-        }
-      }
-      has_liked: idea_likes_aggregate (where: { user: { cognito_sub: {_eq: $cognito_sub}}}) {
-        result: aggregate {
-          count
-        }
-      }
-      idea_comments {
-        id
-        created_at
-        text
-        updated_at
-        user {
-          id
-          first_name
-          last_name
-          avatar
-        }
-      }
+      created_at
+      text
+      updated_at
       user {
         id
-        avatar
         first_name
         last_name
-        role
+        avatar
       }
-      team {
-        id
-        looking_for_members
-        looking_for_mentors
-      }
-      created_at
-      updated_at
     }
+    user {
+      id
+      avatar
+      first_name
+      last_name
+      role
+    }
+    team {
+      id
+      looking_for_members
+      looking_for_mentors
+    }
+    created_at
+    updated_at
   }
-  `;
+}
+`;
 
 const getConnections = `query getConnections($cognito_sub: String) {
-  connections(where: {
-    _or: [
-      { user: {cognito_sub: {_eq: $cognito_sub }}},
-      { userByUser2: {cognito_sub: {_eq: $cognito_sub }}}
-    ]
-  }) {
-    user1
-    user2
-    status
-  }
-  user (where: {cognito_sub: {_eq: $cognito_sub}}) {
-    id
-  }
+connections(where: {
+  _or: [
+    { user: {cognito_sub: {_eq: $cognito_sub }}},
+    { userByUser2: {cognito_sub: {_eq: $cognito_sub }}}
+  ]
+}) {
+  user1
+  user2
+  status
+}
+user (where: {cognito_sub: {_eq: $cognito_sub}}) {
+  id
+}
 }`;
 
 module.exports = {
-    getIdea,
-    getIdeas,
-    getConnections,
+  getIdea,
+  getIdeas,
+  getConnections,
 };
