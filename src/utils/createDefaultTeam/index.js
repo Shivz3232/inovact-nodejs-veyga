e073eb16673c2ddfc8d8009718ca8a46d09170a4
D G Shivu <dgshivu3232@gmail.com>
2023-06-16 17:30:18 +0530

    chore: added missing return and logging

--- src/utils/createDefaultTeam/index.js
+++ src/utils/createDefaultTeam/index.js
@@ -1,13 +1,8 @@
 const { query: Hasura } = require('../hasura');
 const { addMembers, addTeam } = require('./queries/mutations');
+const logger = require('../../config/logger');
 
-async function createDefaultTeam(
-  user_id,
-  name,
-  looking_for_mentors,
-  looking_for_members,
-  team_on_inovact
-) {
+async function createDefaultTeam(user_id, name, looking_for_mentors, looking_for_members, team_on_inovact) {
   const teamData = {
     name,
     creator_id: user_id,
@@ -19,6 +14,7 @@ async function createDefaultTeam(
   const response1 = await Hasura(addTeam, teamData);
 
   if (!response1.success) {
+    logger.error(response1.errors);
     return {
       success: false,
       errorCode: 'InternalServerError',
@@ -40,15 +36,19 @@ async function createDefaultTeam(
   const response2 = await Hasura(addMembers, members);
 
   if (!response2.success) {
+    logger.error(response2.errors);
     return {
       success: false,
       errorCode: 'InternalServerError',
       errorMessage: 'Failed while adding default admin.',
+      data: null,
     };
   }
 
   return {
     success: true,
+    errorCode: '',
+    errorMessage: '',
     team_id: response1.result.data.insert_team.returning[0].id,
   };
 }
