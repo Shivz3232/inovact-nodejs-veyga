00238824a75541ccb8366342f9fe7e055aa70694
Nabeelcodes110 <parvenabeel@gmail.com>
2023-06-26 22:01:10 +0530

    fix project mentions

--- src/controllers/project.controller/addProject/index.js
+++ src/controllers/project.controller/addProject/index.js
@@ -1,11 +1,4 @@
-const {
-  addProject: addProjectQuery,
-  addMentions,
-  addTags,
-  addDocuments,
-  addRolesRequired,
-  addSkillsRequired,
-} = require('./queries/mutations');
+const { addProject: addProjectQuery, addMentions, addTags, addDocuments, addRolesRequired, addSkillsRequired } = require('./queries/mutations');
 const { getUser, getProject } = require('./queries/queries');
 const { query: Hasura } = require('../../../utils/hasura');
 const catchAsync = require('../../../utils/catchAsync');
@@ -43,13 +36,7 @@ const addProject = catchAsync(async (req, res) => {
   if (req.body.team_id) {
     projectData.team_id = req.body.team_id;
   } else if (req.body.looking_for_members || req.body.looking_for_mentors) {
-    teamCreated = await createDefaultTeam(
-      response1.result.data.user[0].id,
-      req.body.team_name ? req.body.team_name : req.body.title + ' team',
-      req.body.looking_for_mentors,
-      req.body.looking_for_members,
-      req.body.team_on_inovact
-    );
+    teamCreated = await createDefaultTeam(response1.result.data.user[0].id, req.body.team_name ? req.body.team_name : req.body.title + ' team', req.body.looking_for_mentors, req.body.looking_for_members, req.body.team_on_inovact);
 
     if (!teamCreated.success) {
       return res.json(teamCreated);
@@ -99,7 +86,7 @@ const addProject = catchAsync(async (req, res) => {
   }
 
   // Insert mentions
-  if (req.body.mentions.length) {
+  if (req.body.mentions) {
     const mentions = req.body.mentions.map((user_id) => {
       return {
         user_id,
