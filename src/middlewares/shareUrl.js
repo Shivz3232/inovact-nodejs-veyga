dfa126ec30d79aca2f3aefb684fe1c56c9a16c70
D G Shivu <dgshivu3232@gmail.com>
2023-07-13 21:48:27 +0530

    chore: remove detect-mobile-browser and added express useragent

--- src/middlewares/shareUrl.js
+++ src/middlewares/shareUrl.js
@@ -1,9 +1,10 @@
-const detectMobileBrowser = require('detect-mobile-browser');
+const UserAgent = require('express-useragent');
 
 const shareUrl = async (req, res, next) => {
-  const userAgent = detectMobileBrowser(req.headers['user-agent']);
+  const userAgent = UserAgent.parse(req.headers['user-agent']);
 
-  let appInstalled = userAgent.isMobile() && userAgent.isBrowser() && userAgent.match();
+  const appInstalled = userAgent.isAndroid && userAgent.isChrome && userAgent.isFirefox;
+
   if (!appInstalled) {
     res.redirect('https://play.google.com/store/apps/details?id=in.pranaydas.inovact');
   } else next();
