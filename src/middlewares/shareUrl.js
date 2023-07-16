488d4ec05dc0a56f664a6f76d66864b00cb04939
Nabeelcodes110 <parvenabeel@gmail.com>
2023-07-16 23:33:07 +0530

    Link sharing updated

--- src/middlewares/shareUrl.js
+++ src/middlewares/shareUrl.js
@@ -3,7 +3,7 @@ const UserAgent = require('express-useragent');
 const shareUrl = async (req, res, next) => {
   const userAgent = UserAgent.parse(req.headers['user-agent']);
 
-  const appInstalled = userAgent.isAndroid && userAgent.isChrome && userAgent.isFirefox;
+  const appInstalled = userAgent.isMobile && userAgent.isAndroid && userAgent.browser === 'unknown';
 
   if (!appInstalled) {
     res.redirect('https://play.google.com/store/apps/details?id=in.pranaydas.inovact');
