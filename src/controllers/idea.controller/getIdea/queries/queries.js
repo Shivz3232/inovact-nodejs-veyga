44330c6417dcba9bdfbcf55332e21c4e117315ec
D G Shivu <dgshivu3232@gmail.com>
2023-08-08 19:05:07 +0530

    chore: added team details to the other query

--- src/controllers/idea.controller/getIdea/queries/queries.js
+++ src/controllers/idea.controller/getIdea/queries/queries.js
@@ -112,6 +112,19 @@ const getIdea = `query getIdea($id: Int, $cognito_sub: String) {
       id
       looking_for_members
       looking_for_mentors
+      name
+      avatar
+      team_members {
+        user {
+          id
+          first_name
+          last_name
+          user_name
+          role
+          admin
+          avatar
+        }
+      }
     }
     created_at
     updated_at
