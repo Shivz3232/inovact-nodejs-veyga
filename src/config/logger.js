9eba470a015dc3dd72a78cd9fbb0ddc9bbb5f130
D G Shivu <dgshivu3232@gmail.com>
2023-08-20 14:16:01 +0530

    chore: update node env var to allow staging

--- src/config/logger.js
+++ src/config/logger.js
@@ -8,13 +8,34 @@ const enumerateErrorFormat = winston.format((info) => {
   return info;
 });
 
+let logLevel;
+switch (config.env) {
+  case 'production':
+    logLevel = 'info';
+    break;
+  case 'staging':
+    logLevel = 'verbose';
+    break;
+  case 'development':
+    logLevel = 'debug';
+    break;
+  default:
+    logLevel = 'debug';
+}
+
 const logger = winston.createLogger({
-  level: config.env === 'development' ? 'debug' : 'info',
+  level: logLevel,
   format: winston.format.combine(
     enumerateErrorFormat(),
     config.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
     winston.format.splat(),
-    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+    winston.format.printf((info) => {
+      if (info.body && typeof info.body === 'object') {
+        info.body = JSON.stringify(info.body, null, 3);
+      }
+
+      return `${info.level}: ${info.message} ${info.body || ''}`;
+    })
   ),
   transports: [
     new winston.transports.Console({
