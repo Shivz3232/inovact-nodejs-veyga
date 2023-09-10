e33061c3151bfe79eb941dbd0ac9de3dc433b648
Pratik <pendurkarpratik04@gmail.com>
2023-09-10 15:51:07 +0530

    Retry finding the user in db after Google Signup

--- src/controllers/user.controller/fetchUser/index.js
+++ src/controllers/user.controller/fetchUser/index.js
@@ -4,7 +4,7 @@ const cleanUserdoc = require('../../../utils/cleanUserDoc');
 const { getUser, getUserById } = require('./queries/queries');
 const catchAsync = require('../../../utils/catchAsync');
 const MAX_RETRY_ATTEMPTS = 3;
-const RETRY_INTERVAL_MS = 5000;
+const RETRY_INTERVAL_MS = 3000;
 
 const fetchUser = catchAsync(async (req, res) => {
   const sanitizerErrors = validationResult(req);
@@ -35,22 +35,30 @@ const fetchUser = catchAsync(async (req, res) => {
       cognito_sub,
     };
   }
-
-  let retryCount = 0;
+  
   let userFound = false;
   let userData = null;
+  let retryCount = 0;
 
-  while (retryCount < MAX_RETRY_ATTEMPTS && !userFound) {
-    const response = await Hasura(query, variables);
+  const response = await Hasura(query, variables);
+  const responseData = response.result.data;
 
-    const responseData = response.result.data;
+  if (responseData || responseData.user.length > 0) {
+    userData = cleanUserdoc(responseData.user[0], responseData.connections[0]);
+    userFound = true;
+  } else {
+    while (retryCount < MAX_RETRY_ATTEMPTS && !userFound) {
+      const response = await Hasura(query, variables);
 
-    if (responseData && responseData.user.length > 0) {
-      userData = cleanUserdoc(responseData.user[0], responseData.connections[0]);
-      userFound = true;
-    } else {
-      await new Promise((resolve) => setTimeout(resolve, RETRY_INTERVAL_MS));
-      retryCount++;
+      const responseData = response.result.data;
+
+      if (responseData && responseData.user.length > 0) {
+        userData = cleanUserdoc(responseData.user[0], responseData.connections[0]);
+        userFound = true;
+      } else {
+        await new Promise((resolve) => setTimeout(resolve, RETRY_INTERVAL_MS));
+        retryCount++;
+      }
     }
   }
 
