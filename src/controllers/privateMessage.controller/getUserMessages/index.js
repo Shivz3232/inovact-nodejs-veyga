383ab6ae19d40ce948c302d5bf5942af5139b159
D G Shivu <dgshivu3232@gmail.com>
2023-09-22 11:38:51 +0530

    chore: sort results based on latest message

--- src/controllers/privateMessage.controller/getUserMessages/index.js
+++ src/controllers/privateMessage.controller/getUserMessages/index.js
@@ -1,7 +1,6 @@
 const { validationResult } = require('express-validator');
 const { query: Hasura } = require('../../../utils/hasura');
-const sortConnections = require('../../../utils/sortConnections');
-const { getUserConnections, getPrivateChats } = require('./queries/queries');
+const { getUserConnections } = require('./queries/queries');
 const catchAsync = require('../../../utils/catchAsync');
 
 const getUserMessages = catchAsync(async (req, res) => {
@@ -17,15 +16,13 @@ const getUserMessages = catchAsync(async (req, res) => {
 
   const response1 = await Hasura(getUserConnections, { cognito_sub });
 
-  const variables = {
-    connection_ids: response1.result.data.connections.map((connection) => connection.id),
-  };
+  const connectionsSortedByLatestMessage = response1.result.data.connections.sort((a, b) => {
+    const aDate = a.private_messages.length ? new Date(a.private_messages[0].created_at) : new Date(0);
+    const bDate = b.private_messages.length ? new Date(b.private_messages[0].created_at) : new Date(0);
+    return bDate - aDate;
+  });
 
-  const response2 = await Hasura(getPrivateChats, variables);
-
-  const connections = sortConnections(response2.result.data.users);
-
-  return res.json(connections);
+  return res.json(connectionsSortedByLatestMessage);
 });
 
 module.exports = getUserMessages;
