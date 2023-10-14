9fadac217f99be1f3829620175bf892e03c6d550
Pratik <pendurkarpratik04@gmail.com>
2023-10-14 22:16:37 +0530

    Sorting users alphabatically

--- src/controllers/user.controller/fetchAllUsers/queries/queries.js
+++ src/controllers/user.controller/fetchAllUsers/queries/queries.js
@@ -1,29 +1,33 @@
 const getAllUserQuery = `
 query getUsers {
-  user (where : {profile_complete :{_eq:true}}){
-    id,
-    user_name,
-    bio,
-    avatar,
-    phone_number,
-    email_id,
-    designation,
-    organization,
-    organizational_role,
-    university,
-    graduation_year,
-    journey_start_date,
-    years_of_professional_experience,
-    created_at,
-    updated_at,
-    first_name,
-    last_name,
-    role,
-    cognito_sub,
-    admin,
+  user(
+    where: { profile_complete: { _eq: true } }
+    order_by: [{ first_name: asc, last_name: asc }]
+  ) {
+    id
+    user_name
+    bio
+    avatar
+    phone_number
+    email_id
+    designation
+    organization
+    organizational_role
+    university
+    graduation_year
+    journey_start_date
+    years_of_professional_experience
+    created_at
+    updated_at
+    first_name
+    last_name
+    role
+    cognito_sub
+    admin
     profile_complete
   }
 }
+
   `;
 
 module.exports = { getAllUserQuery };
