e31797f4e4e3007ff34f74c0bf6f0620286409d3
Pratik <pendurkarpratik04@gmail.com>
2023-11-05 19:07:24 +0530

    Creaternof team can delete remove admins

--- src/controllers/teams.controller/deleteTeamMembers/index.js
+++ src/controllers/teams.controller/deleteTeamMembers/index.js
@@ -1,8 +1,22 @@
 const { validationResult } = require('express-validator');
 const { query: Hasura } = require('../../../utils/hasura');
-const { checkIfCanDelete } = require('./queries/queries.js');
+const { checkIfCanDelete, getUserIdFromCognitoSub } = require('./queries/queries.js');
 const { deleteTeamMember } = require('./queries/mutations');
 const catchAsync = require('../../../utils/catchAsync');
+
+const successResponse = {
+  success: true,
+  errorCode: '',
+  errorMessage: '',
+  data: null,
+};
+
+const errorResponse = (status, errorCode, errorMessage) => ({
+  success: false,
+  errorCode,
+  errorMessage,
+  data: null,
+});
 
 const deleteTeamMembers = catchAsync(async (req, res) => {
   const sanitizerErrors = validationResult(req);
@@ -15,54 +29,35 @@ const deleteTeamMembers = catchAsync(async (req, res) => {
 
   const { user_id, cognito_sub, team_id } = req.body;
 
-  const variables = {
-    team_id,
-    user_id,
-    cognito_sub,
-  };
+  const variables = { team_id, user_id, cognito_sub };
 
   const response1 = await Hasura(checkIfCanDelete, variables);
+  const createrId = response1.result.data.members[0].team.creator_id;
 
-  if (response1.result.data.admins.length == 0) {
-    return res.status(401).json({
-      success: false,
-      errorCode: 'Forbidden',
-      errorMessage: 'You are not an admin of this team.',
-      data: null,
-    });
+  const response2 = await Hasura(getUserIdFromCognitoSub, { cognito_sub });
+  const currentUserId = response2.result.data.user[0].id;
+
+  const variables2 = { user_id, team_id };
+
+  if (response1.result.data.admins.length === 0) {
+    return res.status(401).json(errorResponse('Forbidden', 'You are not an admin of this team.'));
   }
 
-  if (response1.result.data.members.length == 0) {
-    return res.status(400).json({
-      success: false,
-      errorCode: 'Forbidden',
-      errorMessage: 'Given user is not a member of this team.',
-      data: null,
-    });
+  if (response1.result.data.members.length === 0) {
+    return res.status(400).json(errorResponse('Forbidden', 'Given user is not a member of this team.'));
   }
 
   if (response1.result.data.members[0].admin) {
-    return res.status(400).json({
-      success: false,
-      errorCode: 'Forbidden',
-      errorMessage: 'You cannot remove an admin of this team.',
-      data: null,
-    });
+    if (currentUserId === createrId) {
+      await Hasura(deleteTeamMember, variables2);
+      return res.status(200).json(successResponse);
+    } else {
+      return res.status(400).json(errorResponse('Forbidden', 'You cannot remove an admin of this team.'));
+    }
   }
 
-  const variables2 = {
-    user_id,
-    team_id,
-  };
-
-  const response2 = await Hasura(deleteTeamMember, variables2);
-
-  return res.status(200).json({
-    success: true,
-    errorCode: '',
-    errorMessage: '',
-    data: null,
-  });
+  await Hasura(deleteTeamMember, variables2);
+  return res.status(200).json(successResponse);
 });
 
 module.exports = deleteTeamMembers;
--- src/controllers/teams.controller/deleteTeamMembers/queries/queries.js
+++ src/controllers/teams.controller/deleteTeamMembers/queries/queries.js
@@ -2,12 +2,22 @@ const checkIfCanDelete = `query checkIfCanDelete($user_id: Int, $team_id: Int, $
   members: team_members(where: {team_id: {_eq: $team_id}, user_id: {_eq: $user_id}}) {
     user_id
     admin
+    team {
+      creator_id
+    }
   }
   admins: team_members(where: {team_id: {_eq: $team_id}, user: {cognito_sub: {_eq: $cognito_sub}}, admin: {_eq: true}}) {
     user_id
   }
 }`;
 
+const getUserIdFromCognitoSub = `query getUserIdFromCognitoSub($cognito_sub: String) {
+  user(where: {cognito_sub: {_eq: $cognito_sub}}) {
+    id
+  }
+}`;
+
 module.exports = {
   checkIfCanDelete,
+  getUserIdFromCognitoSub,
 };
