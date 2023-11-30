a7b440c03381da6caf0020849d12791cd197bdb2
C0SMlC <pendurkarpratik04@gmail.com>
2023-11-30 11:11:27 +0530

    Add document upload functionality to teams controller

--- src/utils/uploadDocument.js
+++ src/utils/uploadDocument.js
@@ -1,25 +1,21 @@
-const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+// multerConfig.js
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import s3 from './s3';
 
-aws.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'us-west-2',
-});
+const config = require('../config/config');
 
-const s3 = new aws.S3();
+const upload = (foldername, filename) => {
+  return multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: 'my-bucket-name',
+      key: function (req, file, cb) {
+        const fullPath = `inovact-documents/${config.env}/${foldername}/${filename}/${file.originalname}`;
+        cb(null, fullPath);
+      },
+    }),
+  });
+};
 
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: 'my-bucket-name', // Replace with your bucket name
-    acl: 'public-read', // Adjust the ACL according to your needs
-    key: function (request, file, cb) {
-      console.log(file);
-      cb(null, file.originalname); // Use Date.now() for unique file keys
-    },
-  }),
-});
-
-module.exports = upload;
+export default upload;
