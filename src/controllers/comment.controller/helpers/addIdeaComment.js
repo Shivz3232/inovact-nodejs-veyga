fe7eeac07aac5576ea2f247c921974c3cccfad00
C0SMlC <pendurkarpratik04@gmail.com>
2023-12-18 15:23:40 +0530

    Adding editing and delete functionality in the comment controller

--- src/controllers/comment.controller/helpers/addIdeaComment.js
+++
@@ -1,8 +0,0 @@
-const { query: Hasura } = require('../../../utils/hasura');
-const { commentOnIdea } = require('../queries/mutations');
-
-async function addIdeaComment(text, user_id, idea_id) {
-  return await Hasura(commentOnIdea, { text, user_id, idea_id });
-}
-
-module.exports = addIdeaComment;
