a17fb9cf00d219431b7c530ce1175d269ee7742f
C0SMlC <pendurkarpratik04@gmail.com>
2024-01-02 18:06:08 +0530

    removing limit from comments

--- src/controllers/comment.controller/sanitizer.js
+++ src/controllers/comment.controller/sanitizer.js
@@ -4,9 +4,9 @@ const cognito_sub = body('cognito_sub', 'User Not Authorized').exists().isString
 
 const allowedArticleTypes = ['post', 'idea', 'thought'];
 
-const commentSanitizer = [cognito_sub, body('text', 'Invalid Text').isString().isLength({ min: 1, max: 100 }), body('article_id').exists().toInt(), body('article_type').exists().isIn(allowedArticleTypes).withMessage('Invalid articleType')];
+const commentSanitizer = [cognito_sub, body('text', 'Invalid Text').isString(), body('article_id').exists().toInt(), body('article_type').exists().isIn(allowedArticleTypes).withMessage('Invalid articleType')];
 
-const updateCommentSanitizer = [cognito_sub, param('commentId', 'Invalid commentId').exists().isInt(), body('comment', 'Invalid Text').isString().isLength({ min: 1, max: 100 }), body('articleType').exists().isIn(allowedArticleTypes).withMessage('Invalid articleType')];
+const updateCommentSanitizer = [cognito_sub, param('commentId', 'Invalid commentId').exists().isInt(), body('comment', 'Invalid Text').isString(), body('articleType').exists().isIn(allowedArticleTypes).withMessage('Invalid articleType')];
 
 const removeCommentSanitizer = [cognito_sub, param('commentId', 'Invalid commentId').exists().isInt(), body('articleType').exists().isIn(allowedArticleTypes).withMessage('Invalid articleType')];
 
