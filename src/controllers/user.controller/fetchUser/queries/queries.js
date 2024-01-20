fefc0f8b60cad8d1bc2224c554bab5315424d3b0
C0SMlC <pendurkarpratik04@gmail.com>
2024-01-20 12:52:26 +0530

    Returning the tutorial status with fetch user api

--- src/controllers/user.controller/fetchUser/queries/queries.js
+++ src/controllers/user.controller/fetchUser/queries/queries.js
@@ -25,6 +25,11 @@ const getUser = `query getUser($cognito_sub: String) {
     website
     profile_complete,
     status,
+    user_action{
+      profile_tutorial
+      feed_tutorial
+      team_tutorial
+    }
     user_skills {
       id
       skill
@@ -70,6 +75,11 @@ query getUser($id: Int, $cognito_sub: String) {
     cover_photo
     profile_complete,
     status,
+    user_action{
+      profile_tutorial
+      feed_tutorial
+      team_tutorial
+    }
     user_skills {
       id
       skill
