33989dcbe210abdec841b54ae11fc8b3050fbe15
C0SMlC <pendurkarpratik04@gmail.com>
2024-03-28 16:07:22 +0530

    Add comments to address code review suggestions

--- src/utils/appEngagementMonitor/appEngagementMonitor.js
+++ src/utils/appEngagementMonitor/appEngagementMonitor.js
@@ -2,7 +2,7 @@ const { query: Hasura } = require('../hasura');
 
 const userInactivityTracker = async () => {
   try {
-    // TODO: Fetch user from the DB
+    // TODO: Fetch user_actions data from the DB
 
     for (const user of users.rows) {
       const { user_id, last_app_opened_timestamp } = user;
@@ -11,14 +11,14 @@ const userInactivityTracker = async () => {
       const appOpenedToday = last_app_opened_timestamp >= today.setHours(0, 0, 0, 0) && last_app_opened_timestamp <= today.setHours(23, 59, 59, 999);
 
       if (appOpenedToday) {
-        // TODO: Award points to the user
+        // TODO: insert the activity award points to the user
       } else {
         const lastOpenedDate = new Date(last_app_opened_timestamp);
         const timeDiff = today - lastOpenedDate;
         const consecutiveInactiveDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
         if (consecutiveInactiveDays >= 5) {
-          // TODO: decrease points of the user
+          // TODO: insert the activity to decrease points of the user
         }
       }
     }
