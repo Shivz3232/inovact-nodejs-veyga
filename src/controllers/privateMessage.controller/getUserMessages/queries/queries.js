f2175c85fa033d154a33115a4461007e4c6e67fa
C0SMlC <pendurkarpratik04@gmail.com>
2024-05-06 22:56:47 +0530

    update src/controllers/privateMessage.controller/getUserMessages/index.js and queries.js

--- src/controllers/privateMessage.controller/getUserMessages/queries/queries.js
+++ src/controllers/privateMessage.controller/getUserMessages/queries/queries.js
@@ -1,38 +1,60 @@
 const getUserConnections = `query getMyConnections($cognito_sub: String) {
-  connections(where: {_or: [{userByUser2: {cognito_sub: {_eq: $cognito_sub}}, status: {_eq: "pending"}}, {_and: [{status: {_eq: "connected"}, _or: [{user: {cognito_sub: {_eq: $cognito_sub}}}, {userByUser2: {cognito_sub: {_eq: $cognito_sub}}}]}]}]}) {
-    id
-    user1
-    user2
-    status
-    user {
+    connections(
+      where: {
+        _or: [
+          { userByUser2: { cognito_sub: { _eq: $cognito_sub } }, status: { _eq: "pending" } }
+          {
+            _and: [
+              { status: { _eq: "connected" } }
+              {
+                _or: [
+                  { user: { cognito_sub: { _eq: $cognito_sub } } }
+                  { userByUser2: { cognito_sub: { _eq: $cognito_sub } } }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ) {
       id
-      first_name
-      last_name
-      avatar
-      role
-      user_name
+      user1
+      user2
+      status
+      user {
+        id
+        first_name
+        last_name
+        avatar
+        role
+        user_name
+      }
+      userByUser2 {
+        id
+        first_name
+        last_name
+        avatar
+        role
+        user_name
+      }
+      private_messages(order_by: { created_at: desc }, limit: 1) {
+        primary_user_id
+        secondary_user_id
+        encrypted_message
+        created_at
+        seen
+      }
+      private_messages_aggregate(where: { seen: { _eq: false } }) {
+        aggregate {
+          count
+        }
+      }
     }
-    userByUser2 {
+    user(where: { cognito_sub: { _eq: $cognito_sub } }) {
       id
-      first_name
-      last_name
-      avatar
-      role
-      user_name
     }
-    private_messages(order_by: {created_at: desc}, limit: 1) {
-      primary_user_id
-      secondary_user_id
-      encrypted_message
-      created_at
-    }
-  }
-  user(where: {cognito_sub: {_eq: $cognito_sub}}) {
-    id
-  }
-}`;
+  }`;
 
 module.exports = {
   getUserConnections,
 };
-
