2d76e01378232ff568f8efec8554639c2e207b41
D G Shivu <dgshivu3232@gmail.com>
2024-05-16 13:20:49 +0530

    chore: separate map & sort of connections

--- src/controllers/privateMessage.controller/getUserMessages/index.js
+++ src/controllers/privateMessage.controller/getUserMessages/index.js
@@ -4,31 +4,33 @@ const { getUserConnections } = require('./queries/queries');
 const { decryptMessage } = require('../../../utils/decryptMessage');
 const catchAsync = require('../../../utils/catchAsync');
 
-const cleanupResponse = (connections) => {
-  return Promise.all(
-    connections
-      .map(async (connection) => {
-        const { private_messages, ...rest } = connection;
-        const latestMessage = private_messages.length
-          ? {
-              message: await decryptMessage(private_messages[0].encrypted_message),
-              created_at: private_messages[0].created_at,
-              messageSenderId: private_messages[0].primary_user_id,
-              messageReceiverId: private_messages[0].secondary_user_id,
-            }
-          : null;
+const cleanupResponse = async (connections) => {
+  const transformedConnections = await Promise.all(
+    connections.map(async (connection) => {
+      const { private_messages, ...rest } = connection;
+      const latestMessage = private_messages.length
+        ? {
+            message: await decryptMessage(private_messages[0].encrypted_message),
+            created_at: private_messages[0].created_at,
+            messageSenderId: private_messages[0].primary_user_id,
+            messageReceiverId: private_messages[0].secondary_user_id,
+          }
+        : null;
 
-        const numberOfUnreadMessages = connection.private_messages_aggregate.aggregate.count;
-        delete rest.private_messages_aggregate;
+      const numberOfUnreadMessages = connection.private_messages_aggregate.aggregate.count;
+      delete rest.private_messages_aggregate;
 
-        return { ...rest, latestMessage, numberOfUnreadMessages };
-      })
-      .sort((a, b) => {
-        const aDate = a.latestMessage ? new Date(a.latestMessage.created_at) : new Date(0);
-        const bDate = b.latestMessage ? new Date(b.latestMessage.created_at) : new Date(0);
-        return bDate - aDate;
-      })
+      return { ...rest, latestMessage, numberOfUnreadMessages };
+    })
   );
+
+  transformedConnections.sort((a, b) => {
+    const aDate = a.latestMessage ? new Date(a.latestMessage.created_at) : new Date(0);
+    const bDate = b.latestMessage ? new Date(b.latestMessage.created_at) : new Date(0);
+    return bDate - aDate;
+  });
+
+  return transformedConnections;
 };
 
 const getUserMessages = catchAsync(async (req, res) => {
