5ef0c1b784903d99bfa818663dcabf92d25d9731
D G Shivu <dgshivu3232@gmail.com>
2024-06-22 12:56:55 +0530

    chore: implement recommender

--- src/controllers/project.controller/getProject/recommender/index.js
+++ src/controllers/project.controller/getProject/recommender/index.js
@@ -1,17 +1,83 @@
-const { createUserProfile } = require('../userProfile');
+const { TfIdf } = require('natural');
+const { createUserProfile } = require('./userProfile');
 const { createProjectProfile } = require('./projectProfile');
 const { calculateCosineSimilarity } = require('./vectorBasedMapping');
 
-const recommend = (user, projects) => {
-  const userProfile = createUserProfile(user);
-  const projectProfiles = projects.map(createProjectProfile);
+const recommend = async (user, projects) => {
+  // ------------------------------------
+  // Step 1: Prepare user profile
+  // ------------------------------------
+  const userProfile = {
+    profile: await createUserProfile(user),
+  };
 
-  const scoredProjects = projectProfiles.map((projectProfile) => {
-    const score = calculateCosineSimilarity(userProfile, projectProfile);
-    return { ...projectProfile, score };
+  // ------------------------------------
+  // Step 2: Prepare project profiles
+  // ------------------------------------
+  const projectProfiles = new Map();
+  for (let i = 0; i < projects.length; i += 1) {
+    const profile = await createProjectProfile(projects[i]); // Parallelize
+
+    projectProfiles.set(projects[i].id, { profile, project: projects[i] });
+  }
+
+  // ------------------------------------
+  // Step 3: Prepare TF-IDF Corpus
+  // ------------------------------------
+  const tfIdf = new TfIdf();
+
+  let docIdx = 0;
+  projectProfiles.forEach((doc) => {
+    tfIdf.addDocument(doc.profile);
+    doc.tfIdfDocIndex = docIdx;
+
+    docIdx += 1;
   });
 
-  const recommendedProjects = scoredProjects.filter((project) => project.score > 0).sort((a, b) => b.score - a.score);
+  // ------------------------------------
+  // Step 4: Formulate document vecotrs
+  // ------------------------------------
+  projectProfiles.forEach((doc) => {
+    doc.vector = {};
+
+    tfIdf.listTerms(doc.tfIdfDocIndex).forEach((item) => {
+      doc.vector[item.term] = item.tfidf;
+    });
+  });
+
+  // ------------------------------------
+  // Step 5: Formulate query vector
+  // ------------------------------------
+  tfIdf.addDocument(userProfile.profile);
+
+  userProfile.vector = {};
+  tfIdf.listTerms(projects.length).forEach((item) => {
+    userProfile.vector[item.term] = item.tfidf;
+  });
+
+  // ------------------------------------
+  // Step 6: Score projects
+  // ------------------------------------
+  projectProfiles.forEach((doc) => {
+    doc.score = calculateCosineSimilarity(userProfile.vector, doc.vector);
+  });
+
+  // ------------------------------------
+  // Step 7: Sort project based on score
+  // ------------------------------------
+  const projectProfilesSortedByScore = Array.from(projectProfiles)
+    .filter((project) => project[1].score !== 0)
+    .sort((a, b) => b[1].score - a[1].score);
+
+  // projectProfilesSortedByScore.forEach((projectProfile) => {
+  //   console.log(projectProfile[1].score, projectProfile[1].vector)
+  //   console.log();
+  // })
+
+  // ------------------------------------
+  // Step 8: Extract Projects
+  // ------------------------------------
+  const recommendedProjects = projectProfilesSortedByScore.map((project) => project[1].project);
 
   return recommendedProjects;
 };
