fd1145169c25532c6eefae33ed5a0c9c0d518ffc
D G Shivu <dgshivu3232@gmail.com>
2024-06-22 12:56:32 +0530

    chore: fix project tags bug

--- src/controllers/project.controller/getProject/recommender/userProfile.js
+++ src/controllers/project.controller/getProject/recommender/userProfile.js
@@ -1,16 +1,12 @@
-const { getUserInfo } = require('../queries/queries.js');
+const { getUserInfo } = require('../queries/queries');
 const { query: Hasura } = require('../../../../utils/hasura');
 const preprocessUserData = require('../../../../utils/recommendations/preprocessing');
 
 const createUserProfile = async (cognitoSub) => {
   const userInfoQueryResponse = await Hasura(getUserInfo, { cognitoSub });
-  const userProfleBag = [
-    ...userInfoQueryResponse.result.data.user[0].user_skills.map((skill) => skill.skill),
-    ...userInfoQueryResponse.result.data.user[0].user_interests.map(
-      (interest) => interest.area_of_interest.interest
-    ),
-    ...userInfoQueryResponse.result.data.project[0].project_tags.map((tag) => tag.hashtag.name),
-  ];
+  const userProfleBag = [...userInfoQueryResponse.result.data.user[0].user_skills.map((skill) => skill.skill), ...userInfoQueryResponse.result.data.user[0].user_interests.map((interest) => interest.area_of_interest.interest)];
+
+  userInfoQueryResponse.result.data.project.forEach((project) => project.project_tags.forEach((tag) => userProfleBag.push(tag.hashtag.name)));
 
   const userProfileString = userProfleBag.join(' ');
   return preprocessUserData(userProfileString);
