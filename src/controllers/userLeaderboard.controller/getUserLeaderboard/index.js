eef655048cf5f3f06bc06ae128a39ecbfde1f792
D G Shivu <dgshivu3232@gmail.com>
2024-08-14 22:43:29 +0530

    chore: find rank of current user

--- src/controllers/userLeaderboard.controller/getUserLeaderboard/index.js
+++ src/controllers/userLeaderboard.controller/getUserLeaderboard/index.js
@@ -1,6 +1,6 @@
 const { validationResult } = require('express-validator');
 const { query: Hasura } = require('../../../utils/hasura');
-const { getUserLeaderboardQuery } = require('./queries/queries');
+const { getUserLeaderboardQuery, getRankOfUser } = require('./queries/queries');
 const catchAsync = require('../../../utils/catchAsync');
 
 const getUserLeaderboard = catchAsync(async (req, res) => {
@@ -12,35 +12,38 @@ const getUserLeaderboard = catchAsync(async (req, res) => {
     });
   }
 
+  const { cognito_sub } = req.body;
+
   // Get pagination parameters from query string
-  const pageSize = parseInt(req.query.pageSize) || 50;
-  const pageNumber = parseInt(req.query.pageNumber) || 1;
+  const pageSize = parseInt(req.query.pageSize, 10) || 50;
+  const pageNumber = parseInt(req.query.pageNumber, 10) || 1;
 
   // Calculate offset
   const offset = (pageNumber - 1) * pageSize;
 
   const variables = {
     limit: pageSize,
-    offset: offset,
+    offset,
+    cognito_sub,
   };
 
   const response = await Hasura(getUserLeaderboardQuery, variables);
 
-  const userPoints = response.result.data.user_points;
-
-  const dataWithRank = userPoints.map((entry, index) => ({
-    rank: index + 1,
-    first_name: entry.user.first_name,
-    last_name: entry.user.last_name,
-    role: entry.user.role,
-    organization: entry.user.organization,
-    avatar: entry.user.avatar,
-    points: entry.points,
-  }));
+  const currentUserId = response.result.data.currentUser[0].user.id;
+  const currentUserRankResponse = await Hasura(getRankOfUser, { user_id: currentUserId });
 
   return res.json({
     success: true,
-    data: dataWithRank,
+    data: {
+      leaderBoard: response.result.data.user_points.map((rec) => {
+        return { ...rec.user, points: rec.points };
+      }),
+      currentUser: {
+        ...response.result.data.currentUser[0].user,
+        points: response.result.data.currentUser[0].points,
+        rank: currentUserRankResponse.result.data.user_rank[0].rank,
+      },
+    },
   });
 });
 
