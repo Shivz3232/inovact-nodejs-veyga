46b76e1b3147cc47d329333b2202d15cdf8bf8fd
Pratik Sunil Pendurkar <pendurkarpratik04@gmail.com>
2025-01-30 22:47:25 +0530

    feat: return connections and team joined count

--- src/utils/cleanUserDoc.js
+++ src/utils/cleanUserDoc.js
@@ -1,23 +1,30 @@
-function cleanUserdoc(userDoc, connection) {
+function cleanUserdoc(userDoc, connection, options = {}) {
   const temp = new Date(userDoc.graduation_year);
-
   if (!Number.isNaN(temp)) userDoc.graduation_year = temp.getFullYear();
 
   const connections_status = connection ? connection.status : 'not connected';
   const sender_id = connection ? connection.sender_id : -1;
 
-  return {
+  const cleanedDoc = {
     ...userDoc,
     success: true,
-    user_interests: userDoc.user_interests.map((user_interest) => {
-      return {
-        id: user_interest.area_of_interest.id,
-        interest: user_interest.area_of_interest.interest,
-      };
-    }),
+    user_interests: userDoc.user_interests.map((user_interest) => ({
+      id: user_interest.area_of_interest.id,
+      interest: user_interest.area_of_interest.interest,
+    })),
     connections_status,
     sender_id,
   };
+
+  if (options.connectionsCount !== undefined) {
+    cleanedDoc.numOfConnections = options.connectionsCount;
+  }
+
+  if (options.teamsCount !== undefined) {
+    cleanedDoc.teamsJoined = options.teamsCount;
+  }
+
+  return cleanedDoc;
 }
 
 module.exports = cleanUserdoc;
