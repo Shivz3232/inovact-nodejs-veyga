430e39a8a7a8ddd5deac50f75d7c9d1017d4187c
Pratik Sunil Pendurkar <pendurkarpratik04@gmail.com>
2025-07-16 21:48:36 +0530

    feat: simplify degree assignment logic in update user function

--- src/controllers/user.controller/updateAnUser/index.js
+++ src/controllers/user.controller/updateAnUser/index.js
@@ -79,11 +79,7 @@ const updateanUser = catchAsync(async (req, res) => {
   if (req.body.years_of_professional_experience)
     variables.changes.years_of_professional_experience = req.body.years_of_professional_experience;
   if (req.body.degree) {
-    if (req.body.role && req.body.role === 'student') {
-      variables.changes.degree = req.body.designation;
-    } else {
-      variables.changes.degree = req.body.degree;
-    }
+    variables.changes.degree = req.body.degree;
   }
   if (req.body.github_profile) {
     variables.changes.github_profile = req.body.github_profile;
